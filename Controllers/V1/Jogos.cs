0cd02983dd396d545a74b15b7e29e4f5da5f0cf8
JosiasLopes <jobercardinho2014@gmail.com>
2021-07-22 21:42:35 -0300

    Primeiro commit

---
+++ Controllers/V1/Jogos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catalogo_de_jogos_dotnet_core.Controllers.V1
+{
+    //inicio...
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class Jogos : ControllerBase
+    {
+        //Action result diz quais são os retornos, status e etc
+        public async Task<ActionResult<List<Object>>> Obter()
+        {
+
+        }
+    }
+}
