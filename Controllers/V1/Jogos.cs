5e54f86b662867aa7e14bb5332a35daae39f2f89
JosiasLopes <jobercardinho2014@gmail.com>
2021-07-24 20:30:35 -0300

    Alteração da classe Jogos, Criação das classes Dto. JogoInputModel, JogoViewModel

--- Controllers/V1/Jogos.cs
+++ Controllers/V1/Jogos.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using catalogo_de_jogos_dotnet_core.DTO.InputModel;
+using catalogo_de_jogos_dotnet_core.DTO.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -14,42 +16,42 @@ namespace catalogo_de_jogos_dotnet_core.Controllers.V1
     {
         //Action result diz quais são os retornos, status e etc
         [HttpGet]
-        public async Task<ActionResult<List<Object>>> listarJogos()
+        public async Task<ActionResult<List<JogoViewModel>>> listarJogos()
         {
 
             return Ok();
         }
 
         [HttpGet("(idjogo:guid)")]
-        public async Task<ActionResult<Object>> getJogoById(Guid idjogo)
+        public async Task<ActionResult<JogoViewModel>> getJogoById(Guid idjogo)
         {
 
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<Object>> postJogo(object jogo)
+        public async Task<ActionResult<JogoViewModel>> postJogo(JogoInputModel jogo)
         {
 
             return Ok();
         }
 
         [HttpPut("(idjogo:guid)")]  //o put atualiza todo o recurso
-        public async Task<ActionResult<Object>> updateJogo(Guid idjogo,object jogo)
+        public async Task<ActionResult<JogoViewModel>> updateJogo(Guid idjogo, JogoInputModel jogo)
         {
 
             return Ok();
         }
 
         [HttpPut("(idjogo:guid)/preco/(preco:double)")] //atualiza um campo especifico
-        public async Task<ActionResult<Object>> patchJogo(Guid idjogo, double jogo)
+        public async Task<ActionResult<JogoViewModel>> patchJogo(Guid idjogo, double jogo)
         {
             
             return Ok();
         }
 
         [HttpDelete("(idjogo:guid)")]  //o put atualiza todo o recurso
-        public async Task<ActionResult<Object>> deleteJogo(Guid idjogo)
+        public async Task<ActionResult<JogoViewModel>> deleteJogo(Guid idjogo)
         {
 
             return Ok();
---
+++ DTO/InputModel/JogoInputModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catalogo_de_jogos_dotnet_core.DTO.InputModel
+{
+    public class JogoInputModel
+    {
+        //Essa classe usa o conceito de Fast Fail onde caso a requisição detecte o erro ela nem entrara no controller
+        //dessa forma se poupa recursos computacionais
+       
+        [Required]  //essa anotatio diz que o atribto é obrigatorio
+        [StringLength(100,MinimumLength =3,ErrorMessage ="O nome deve ter entre 3 e 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "A Produtora deve ter entre 3 e 100 caracteres")]
+        public string Produtora { get; set; }
+
+        [Required]
+        [Range(1,100,ErrorMessage ="O preço deve estar entre 1 e 1000 Reais")]
+        public double Preco { get; set; }
+    }
+}
